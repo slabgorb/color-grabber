890b40cb96a00b283dc5de44d0ba817c2291abf7
Keith Avery <slabgorb@gmail.com>
2015-10-16 15:38:45 -0400

    shortened example html

--- test/test_kuler-export.rb
+++ test/test_kuler-export.rb
@@ -6,7 +6,7 @@ class TestKulerExport < Test::Unit::TestCase
   def test_find_colors
     ke = KulerExport.new('foo', Nokogiri::HTML(open('test/example.html')))
     assert_equal(["#292929", "#5B7876", "#8F9E8B", "#F2E6B6", "#412A22"], ke.colors)
-    assert_equal("$foo_0: #292929\n$foo_1: #5B7876\n$foo_2: #8F9E8B\n$foo_3: #F2E6B6\n$foo_4: #412A22", ke.to_sass)
+    assert_equal("$foo_1: #292929\n$foo_2: #5B7876\n$foo_3: #8F9E8B\n$foo_4: #F2E6B6\n$foo_5: #412A22", ke.to_sass)
     assert_equal("#292929\n#5B7876\n#8F9E8B\n#F2E6B6\n#412A22", ke.to_s)
   end
 end
